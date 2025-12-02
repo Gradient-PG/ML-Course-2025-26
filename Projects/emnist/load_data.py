16b546951188c1a276f777003a2f51f90bda1d11
microslaw <milosz.grunwald@gmail.com>
2025-12-02 17:15:16 +0100

    fix: change mnist project

--- Projects/emnist/load_data.py
+++ Projects/emnist/load_data.py
@@ -1,8 +1,7 @@
-# Create a pytorch neural network that will classify digits
+# Create a pytorch neural network that will classify digits and letters
 # below you can see how to load required data
 # you can iterate over the dataset to get data-label pairs
 
 import torchvision
-
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-dataset = torchvision.datasets.MNIST(".", download=True, transform=transform)
+dataset = torchvision.datasets.EMNIST(".", download=True, transform=transform, split="balanced")
